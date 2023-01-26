00d4a071ecffca2d7bed85636c2f24835ae96e35
Kacper Janus <kacperj07@icloud.com>
2023-01-26 12:53:59 +0100

    added bounds to zooming and panning

--- folium-meteor-map/main.py
+++ folium-meteor-map/main.py
@@ -6,14 +6,20 @@ from meteor_icon import create_meteor_icon
 from description import describe
 
 # Create a map object centered on the point of interest
-m = folium.Map(location=[20, 0], zoom_start=2, min_zoom=1, max_zoom=20)
-m.fit_bounds([[-90, -180], [90, 180]])
+m = folium.Map(location=[20, 0],
+               zoom_start=2,
+               max_bounds=True,
+               )
+# Adding a TileLayer that determines map's visual appearance
 folium.TileLayer('https://basemap.nationalmap.gov/arcgis/rest/services/USGSTopo/MapServer/tile/{z}/{y}/{x}',
                  name='USGS.USTopo',
-                 attr='Tiles courtesy of the <a href="https://usgs.gov/">U.S. Geological Survey</a>').add_to(m)
+                 attr='Tiles courtesy of the <a href="https://usgs.gov/">U.S. Geological Survey</a>',
+                 min_zoom=1.6,
+                 max_zoom=18
+                 ).add_to(m)
 
 # Create a marker cluster object
-mc = MarkerCluster(max_cluster_radius=84, show_coverage_on_hover=True, disableClusteringAtZoom=6)
+mc = MarkerCluster(max_cluster_radius=90, show_coverage_on_hover=True, disableClusteringAtZoom=6)
 
 # Reading from the meteorite_data.json and ignoring rows without the geolocation
 df = pd.DataFrame(pd.read_json("meteorite_data.json"))
@@ -35,6 +41,7 @@ for i in range(n):
     mc.add_child(marker)
 m.add_child(mc)
 # Save the map object to an HTML file
+
 m.save('my_map.html')
 
 # Open the HTML file in a web browser
